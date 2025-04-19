8248ed2d4ab08be0ee548d2c99e327fb0e2aa305
ffxlapen2 <ffxlapen@gmail.com>
2025-04-19 17:10:01 +0900

    initial commit

--- app/src/main/java/com/example/loginapp/profilactivity.kt
+++ app/src/main/java/com/example/loginapp/profilactivity.kt
@@ -1,6 +1,9 @@
 package com.example.loginapp
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ProfilActivity : AppCompatActivity() {
@@ -10,9 +13,24 @@ class ProfilActivity : AppCompatActivity() {
 
         supportActionBar?.title = "Profil"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // Instagram dan WhatsApp
+        val igTextView = findViewById<TextView>(R.id.profile_instagram)
+        val waTextView = findViewById<TextView>(R.id.profile_whatsapp)
+
+        igTextView.setOnClickListener {
+            val igIntent = Intent(Intent.ACTION_VIEW)
+            igIntent.data = Uri.parse("https://instagram.com/ferlyyyyyyyyyy") // Ganti sesuai username IG
+            startActivity(igIntent)
+        }
+
+        waTextView.setOnClickListener {
+            val waIntent = Intent(Intent.ACTION_VIEW)
+            waIntent.data = Uri.parse("https://wa.me/081248471001") // Ganti dengan nomor WhatsApp
+            startActivity(waIntent)
+        }
     }
 
-    // Menangani tombol back di action bar
     override fun onSupportNavigateUp(): Boolean {
         finish()
         return true
